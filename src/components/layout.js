8a823ead0d661504bcbdf045b9bc9f980b7c82e1
Kevin B. Ridgway <kridgway@gmail.com>
2018-11-27 22:46:42 -0500

    Updating to make homepage more clear.

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,32 +11,86 @@ class Template extends React.Component {
 
     if (location.pathname === rootPath) {
       header = (
-        <h1
+        <React.Fragment>
+          <h1
+            style={{
+              ...scale(1.5),
+              marginBottom: rhythm(.5),
+              width: '100vw',
+              textAlign: 'center',
+              padding: '.05em',
+            }}
+          >
+            <Link
+              style={{
+                boxShadow: 'none',
+                textDecoration: 'none',
+                color: 'inherit',
+              }}
+              to={'/'}
+            >
+              100 Days of Sketches
+            </Link>
+          </h1>
+          <hr style={{
+            ...scale(0.2),
+            marginBottom: '2em',
+            width: '75vw',
+            textAlign: 'center',
+            margin: '0 auto',
+            height: '1px',
+          }} />
+          <p
           style={{
-            ...scale(1.5),
-            marginBottom: rhythm(1.5),
-            marginTop: 0,
+            ...scale(0.2),
+            paddingTop: '2em',
+            width: '75vw',
+            textAlign: 'center',
+            margin: '0 auto'
           }}
         >
-          <Link
-            style={{
-              boxShadow: 'none',
-              textDecoration: 'none',
-              color: 'inherit',
-            }}
-            to={'/'}
-          >
-            100 Days of Sketches
-          </Link>
-        </h1>
+        Using the awesome <a href="https://linea-app.com/?ref=100daysofsketches.com">Linea Sketch app</a> on my trusty iPad Pro, let's see if I can sketch for 100 days, with the following constraints:
+        </p>
+        <div
+        style={{
+          ...scale(0.2),
+          width: '75vw',
+          margin: '0 auto',
+          marginTop: '1em',
+          textAlign: 'center'
+        }}
+      >
+          <ol
+          style={{
+            ...scale(0.1),
+            marginBottom: rhythm(.85),
+            width: '35vw',
+            margin: '0 auto',
+            textAlign: 'left',
+          }}
+        >
+            <li>
+              Using only the pencil tool
+            </li>
+            <li>Only using the color black</li>
+            <li>No extensive layers use, 2 layers max</li>
+            <li> No straight line tool, scissors, move tool, or any "assistance" tools used</li>
+            <li>Erase, undo/redo tools are ok to use</li>
+            <li>Less than 20 minutes spent on each sketch</li>
+            <li>No sketch idea can repeat</li>
+          </ol>
+        </div>
+      </React.Fragment>
       )
     } else {
       header = (
         <h3
           style={{
             fontFamily: 'Montserrat, sans-serif',
-            marginTop: 0,
             marginBottom: rhythm(-1),
+            width: '100vw',
+            textAlign: 'center',
+            padding: '.05em',
           }}
         >
           <Link
@@ -53,6 +107,8 @@ class Template extends React.Component {
       )
     }
     return (
+      <React.Fragment>
+      {header}
       <div
         style={{
           marginLeft: 'auto',
@@ -61,9 +117,9 @@ class Template extends React.Component {
           padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
         }}
       >
-        {header}
         {children}
       </div>
+      </React.Fragment>
     )
   }
 }
